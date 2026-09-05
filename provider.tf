77787aaf09328c1a18211eb86419524d48805b2e
rajnish2323 <rajnishk126@gmail.com>
2026-09-05 18:56:06 +0530

    Update Azure Terraform configuration

--- provider.tf
+++ provider.tf
@@ -15,3 +15,10 @@ resource "azurerm_resource_group" "rg" {
   name = "raj"
   location = "centralindia"
 }
+resource "azurerm_virtual_network" "vnet"{
+  name = "vnet01"
+ resource_group_name = azurerm_resource_group.rg.name
+ requ
+ address_spaces = ["1.0.0.0/24"]
+
+}
