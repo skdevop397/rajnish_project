8370673713581390a94cbbe52de2331087c39dc0
rajnish2323 <rajnishk126@gmail.com>
2026-09-05 19:11:10 +0530

    more_subnet

--- provider.tf
+++ provider.tf
@@ -34,3 +34,15 @@ resource "azurerm_subnet" "snet" {
   resource_group_name = "azurerm_resource_group.rg.name"
   address_prefixes = ["10.0.2.0/24"]
 }
+resource "azurerm_subnet" "snet" {
+  name = "snet2"
+  virtual_network_name = "azurerm_virtual_network.vnet.name"
+  resource_group_name = "azurerm_resource_group.rg.name"
+  address_prefixes = ["10.0.2.0/24"]
+}
+resource "azurerm_subnet" "snet" {
+  name = "snet2"
+  virtual_network_name = "azurerm_virtual_network.vnet.name"
+  resource_group_name = "azurerm_resource_group.rg.name"
+  address_prefixes = ["10.0.2.0/24"]
+}
