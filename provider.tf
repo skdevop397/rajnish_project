01383a1a7cf8c99713f6426d2c2bb774bd129517
SK Sharma <skdevop.23@gmail.com>
2026-09-05 19:07:08 +0530

    snet added

--- provider.tf
+++ provider.tf
@@ -18,7 +18,19 @@ resource "azurerm_resource_group" "rg" {
 resource "azurerm_virtual_network" "vnet"{
   name = "vnet01"
  resource_group_name = azurerm_resource_group.rg.name
- requ
- address_spaces = ["1.0.0.0/24"]
-
+ location = azurerm_resource_group.rg.location
+ address_space = ["10.0.0.0/16"]
+}
+resource "azurerm_subnet" "snet" {
+  name = "snet1"
+  virtual_network_name = "azurerm_virtual_network.vnet.name"
+  resource_group_name = "azurerm_resource_group.rg.name"
+  address_prefixes = ["10.0.1.0/24"]
+  
+}
+resource "azurerm_subnet" "snet" {
+  name = "snet2"
+  virtual_network_name = "azurerm_virtual_network.vnet.name"
+  resource_group_name = "azurerm_resource_group.rg.name"
+  address_prefixes = ["10.0.2.0/24"]
 }
