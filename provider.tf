18fb1c1ec7733501c3cc5fc5077f4904902b060b
SK Sharma <skdevop.23@gmail.com>
2026-09-05 19:13:51 +0530

    deleted subnet

--- provider.tf
+++ provider.tf
@@ -28,21 +28,3 @@ resource "azurerm_subnet" "snet" {
   address_prefixes = ["10.0.1.0/24"]
   
 }
-resource "azurerm_subnet" "snet" {
-  name = "snet2"
-  virtual_network_name = "azurerm_virtual_network.vnet.name"
-  resource_group_name = "azurerm_resource_group.rg.name"
-  address_prefixes = ["10.0.2.0/24"]
-}
-resource "azurerm_subnet" "snet" {
-  name = "snet2"
-  virtual_network_name = "azurerm_virtual_network.vnet.name"
-  resource_group_name = "azurerm_resource_group.rg.name"
-  address_prefixes = ["10.0.2.0/24"]
-}
-resource "azurerm_subnet" "snet" {
-  name = "snet2"
-  virtual_network_name = "azurerm_virtual_network.vnet.name"
-  resource_group_name = "azurerm_resource_group.rg.name"
-  address_prefixes = ["10.0.2.0/24"]
-}
