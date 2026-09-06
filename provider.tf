54fbf8ad985c8607e1c9cd65f4678e01f6792cf2
rajnish2323 <rajnishk126@gmail.com>
2026-09-06 10:04:45 +0530

    Add Azure VM Terraform code

--- provider.tf
+++ provider.tf
@@ -1,30 +1,170 @@
-terraform{
-    required_providers {
-      azurerm={
-      source = "hashicorp/azurerm"
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
       version = "5.3.0"
-      }
     }
-}
-provider "azurerm" {
-  features {
-    
   }
 }
+
+provider "azurerm" {
+  features {}
+}
+
+# -------------------------
+# Resource Group
+# -------------------------
 resource "azurerm_resource_group" "rg" {
-  name = "raj"
-  location = "centralindia"
+  name     = "raj"
+  location = "Central India"
 }
-resource "azurerm_virtual_network" "vnet"{
-  name = "vnet01"
- resource_group_name = azurerm_resource_group.rg.name
- location = azurerm_resource_group.rg.location
- address_space = ["10.0.0.0/16"]
+
+# -------------------------
+# Virtual Network
+# -------------------------
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet01"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+
+  address_space = ["10.0.0.0/16"]
 }
-resource "azurerm_subnet" "snet" {
-  name = "snet1"
-  virtual_network_name = "azurerm_virtual_network.vnet.name"
-  resource_group_name = "azurerm_resource_group.rg.name"
+
+# -------------------------
+# Subnet 1
+# -------------------------
+resource "azurerm_subnet" "snet1" {
+  name                 = "snet1"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+
   address_prefixes = ["10.0.1.0/24"]
-  
+}
+
+# -------------------------
+# Subnet 2
+# -------------------------
+resource "azurerm_subnet" "snet2" {
+  name                 = "snet2"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+
+  address_prefixes = ["10.0.2.0/24"]
+}
+
+# -------------------------
+# Public IP
+# -------------------------
+resource "azurerm_public_ip" "pip" {
+  name                = "vm-public-ip"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+
+  allocation_method = "Static"
+  sku               = "Standard"
+}
+
+# -------------------------
+# Network Security Group
+# -------------------------
+resource "azurerm_network_security_group" "nsg" {
+  name                = "vm-nsg"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+
+  # SSH
+  security_rule {
+    name                       = "Allow-SSH"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  # HTTP
+  security_rule {
+    name                       = "Allow-HTTP"
+    priority                   = 110
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+# -------------------------
+# Network Interface
+# -------------------------
+resource "azurerm_network_interface" "nic" {
+  name                = "vm-nic"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.snet1.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.pip.id
+  }
+}
+
+# -------------------------
+# Attach NSG to NIC
+# -------------------------
+resource "azurerm_network_interface_security_group_association" "nsg_association" {
+  network_interface_id      = azurerm_network_interface.nic.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+# -------------------------
+# Linux Virtual Machine
+# -------------------------
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "raj-vm"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_B1s"
+
+  admin_username = "azureuser"
+
+  # SSH public key
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
+
+  network_interface_ids = [
+    azurerm_network_interface.nic.id
+  ]
+
+  os_disk {
+    name                 = "raj-vm-osdisk"
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "ubuntu-24_04-lts"
+    sku       = "server"
+    version   = "latest"
+  }
+}
+
+# -------------------------
+# Output Public IP
+# -------------------------
+output "vm_public_ip" {
+  value = azurerm_public_ip.pip.ip_address
+}
+
+output "vm_private_ip" {
+  value = azurerm_network_interface.nic.private_ip_address
 }
